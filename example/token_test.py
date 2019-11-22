c916dcf09ec69e60eed7e53b2c9f613f6e1a12c3
niuliangtao@youzan.com <niuliangtao@youzan.com>
2019-11-22 15:42:49 +0800

    add

--- example/token_test.py
+++
@@ -1,7 +0,0 @@
-from notedrive.baidu.token_ import AccessToken
-
-token = AccessToken()
-# token = AccessToken()
-# token.init_refresh_token()
-token.refresh_access_token()
-print("access_token\t" + token.secret['token']['access_token'])
--- script/push.sh
+++ script/push.sh
@@ -1,4 +1,4 @@
 git pull
 git add -A
-git commit -a -m "init"
+git commit -a -m "add"
 git push
