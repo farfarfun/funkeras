940ad7750eb62b7de66007a2d26ee7cc62178e3d
niuliangtao@youzan.com <niuliangtao@youzan.com>
2019-11-22 16:07:44 +0800

    add

---
+++ example/TransExample.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+# from keras_transformer import get_model
+from ..transformer import get_model
+
+# 构建一个toy词典
+tokens = 'all work and no play makes jack a dull boy'.split(' ')
+token_dict = {
+    '<PAD>': 0,
+    '<START>': 1,
+    '<END>': 2,
+}
+for token in tokens:
+    if token not in token_dict:
+        token_dict[token] = len(token_dict)
+
+# 生成toy数据
+encoder_inputs_no_padding = []
+encoder_inputs, decoder_inputs, decoder_outputs = [], [], []
+for i in range(1, len(tokens) - 1):
+    encode_tokens, decode_tokens = tokens[:i], tokens[i:]
+    encode_tokens = ['<START>'] + encode_tokens + ['<END>'] + ['<PAD>'] * (len(tokens) - len(encode_tokens))
+    output_tokens = decode_tokens + ['<END>', '<PAD>'] + ['<PAD>'] * (len(tokens) - len(decode_tokens))
+    decode_tokens = ['<START>'] + decode_tokens + ['<END>'] + ['<PAD>'] * (len(tokens) - len(decode_tokens))
+    encode_tokens = list(map(lambda x: token_dict[x], encode_tokens))
+    decode_tokens = list(map(lambda x: token_dict[x], decode_tokens))
+    output_tokens = list(map(lambda x: [token_dict[x]], output_tokens))
+    encoder_inputs_no_padding.append(encode_tokens[:i + 2])
+    encoder_inputs.append(encode_tokens)
+    decoder_inputs.append(decode_tokens)
+    decoder_outputs.append(output_tokens)
+
+# 构建模型
+model = get_model(
+    token_num=len(token_dict),
+    embed_dim=30,
+    encoder_num=3,
+    decoder_num=2,
+    head_num=3,
+    hidden_dim=120,
+    attention_activation='relu',
+    feed_forward_activation='relu',
+    dropout_rate=0.05,
+    embed_weights=np.random.random((13, 30)),
+)
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+)
+model.summary()
+
+# Train the model
+model.fit(
+    x=[np.asarray(encoder_inputs * 1000), np.asarray(decoder_inputs * 1000)],
+    y=np.asarray(decoder_outputs * 1000),
+    epochs=5,
+)
